db5592ef30a1b0610fae760026260d16b1e5773e
Matheu5Martins <dev.sincroni@gmail.com>
2021-02-25 16:31:01 -0300

    first commit

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    // API reference URL
+    apiUrl: "https://eu275.chat-api.com/instance232639/",
+    // API token from your personal cabinet
+    token: "reg8km5t57o4z7rl"
+}
